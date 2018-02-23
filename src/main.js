f35a2af0a187d05683f723b8ddeb9e2e94a09e85
FateZeros <490293266@qq.com>
2018-02-23 17:57:57 +0800

    init web_ouji

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
